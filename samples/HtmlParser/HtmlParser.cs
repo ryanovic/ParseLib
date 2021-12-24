d0fc1d461791aa3fb40e1fe50194e3885caa6e74
Raman Y <immortalGAD@gmail.com>
2021-12-24 03:21:57 +0300

    add HTML sample

---
+++ samples/HtmlParser/HtmlParser.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+using ParseLib.Runtime;
+
+namespace HtmlParser
+{
+    public abstract class HtmlParser : TextParser
+    {
+        private readonly Stack<XmlElement> elements;
+        public XmlDocument Document { get; }
+
+        public HtmlParser(TextReader reader) : base(reader)
+        {
+            elements = new Stack<XmlElement>();
+            Document = new XmlDocument();
+        }
+
+        [CompleteToken("comment")]
+        protected XmlNode CreateComment() => Document.CreateComment(GetLexeme(trimLeft: 4, trimRight: 3));
+
+        [CompleteToken("text")]
+        protected XmlNode CreateText() => Document.CreateTextNode(WebUtility.UrlDecode(GetLexeme()));
+
+        [CompleteToken("%script%")]
+        protected XmlNode CreateScript() => Document.CreateCDataSection(GetLexeme());
+
+        [CompleteToken("<script")]
+        [CompleteToken("<tag")]
+        protected XmlElement CreateElement() => Document.CreateElement(GetLexeme(trimLeft: 1, trimRight: 0));
+
+        [CompleteToken("</tag")]
+        protected string CreateCloseElement() => GetLexeme(trimLeft: 2, trimRight: 0);
+
+        [CompleteToken("attr-name")]
+        protected string CreateAttributeName() => GetLexeme();
+
+        [CompleteToken("attr-value-raw")]
+        protected string CreateAttributeRawValue() => GetLexeme();
+
+        [CompleteToken("attr-value-str")]
+        protected string CreateAttributeStringValue() => GetLexeme(trim: 1);
+
+        [Reduce("attr:single")]
+        protected XmlElement AppendAttribute(XmlElement element, string name)
+        {
+            element.SetAttribute(name, name);
+            return element;
+        }
+
+        [Reduce("attr:value-raw")]
+        [Reduce("attr:value-str")]
+        protected XmlElement AppendAttribute(XmlElement element, string name, string value)
+        {
+            element.SetAttribute(name, value);
+            return element;
+        }
+
+        [Reduce("node:script-inline")]
+        protected void AppendScript(XmlElement element, XmlNode script)
+        {
+            element.AppendChild(script);
+            AppendElement(element);
+        }
+
+
+        [Reduce("node:script-single")]
+        [Reduce("node:tag-single")]
+        protected void AppendElement(XmlElement element)
+        {
+            GetElement().AppendChild(element);
+        }
+
+        [Reduce("node:comment")]
+        [Reduce("node:text")]
+        protected void AppendNode(XmlNode node)
+        {
+            GetElement().AppendChild(node);
+        }
+
+        [Reduce("node:tag-open")]
+        protected void BeginElement(XmlElement element)
+        {
+            if (Document.DocumentElement == null)
+            {
+                Document.AppendChild(element);
+            }
+            else
+            {
+                AppendElement(element);
+            }
+
+            if (!element.Name.Equals("br", StringComparison.OrdinalIgnoreCase))
+            {
+                elements.Push(element);
+            }
+        }
+
+        [Reduce("node:tag-close")]
+        protected void CompleteElement(string name)
+        {
+            while (elements.Count > 0 && !elements.Peek().Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+            {
+                elements.Pop();
+            }
+
+            if (elements.Count == 0)
+            {
+                throw CreateParserException("unexpected close tag encountered.");
+            }
+
+            elements.Pop();
+        }
+
+        private XmlElement GetElement()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Root element is missing.");
+            }
+
+            return elements.Peek();
+        }
+    }
+}
---
+++ samples/HtmlParser/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using ParseLib;
+
+namespace HtmlParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grammar = CreateGrammar();
+            var factory = grammar.CreateTextParserFactory<HtmlParser>("nodes");
+            var parser = (HtmlParser)null;
+
+            using (var reader = new StreamReader("input.htm"))
+            {
+                parser = factory(reader);
+                parser.Parse();
+            }
+
+            var settings = new XmlWriterSettings { Indent = true };
+
+            using (var writer = XmlWriter.Create(Console.Out, settings))
+            {
+                parser.Document.WriteContentTo(writer);
+            }
+        }
+
+        static Grammar CreateGrammar()
+        {
+            var ws = Rex.Char(@" \t\r\n");
+            var name_char = Rex.Char(@"a-z0-9-_");
+
+            var text_char = Rex.IfNot(Rex.Or(
+                Rex.Text("<!--"),
+                Rex.Char('<').Then(name_char),
+                Rex.Text("</").Then(name_char))).Then(Rex.AnyChar);
+            var script_char = Rex.IfNot("</script").Then(Rex.AnyChar);
+
+            var tag_open = Rex.Char('<').Then(name_char.OneOrMore());
+            var tag_close = Rex.Text("</").Then(name_char.OneOrMore());
+
+            var grammar = new Grammar(ignoreCase: true);
+
+            grammar.CreateNonTerminals("node", "nodes", "tag", "attr", "attrs");
+            grammar.CreateTerminals("=", "/>", ">", "<script", "</script");
+            grammar.CreateWhitespace("ws", ws.OneOrMore());
+
+            grammar.CreateTerminal("node:comment", Rex.Text("<!--").Then(Rex.AnyText).Then("-->"), lazy: true);
+            grammar.CreateTerminal("node:text", text_char.OneOrMore());
+
+            grammar.CreateTerminal("%script%", script_char.OneOrMore());
+            grammar.CreateTerminal("<tag", tag_open);
+            grammar.CreateTerminal("</tag", tag_close);
+            grammar.CreateTerminal("attr-name", name_char.OneOrMore());
+            grammar.CreateTerminal("attr-value-raw", name_char.OneOrMore());
+            grammar.CreateTerminal("attr-value-str", Rex.Char('"').Then(Rex.AnyText).Then('"'), lazy: true);
+
+            grammar.AddRule("attr:single", "attr-name");
+            grammar.AddRule("attr:value-raw", "attr-name = attr-value-raw");
+            grammar.AddRule("attr:value-str", "attr-name = attr-value-str");
+
+            grammar.AddRule("attrs:empty", "").ShiftOn("attr-name");
+            grammar.AddRule("attrs:init", "attr");
+            grammar.AddRule("attrs:append", "attrs attr");
+
+            grammar.AddRule("node:script-single", "<script attrs />");
+            grammar.AddRule("node:script-inline", "<script attrs > %script% </script >");
+
+            grammar.AddRule("node:tag-single", "<tag attrs />");
+            grammar.AddRule("node:tag-open", "<tag attrs >");
+            grammar.AddRule("node:tag-close", "</tag >");
+
+            grammar.AddRule("nodes:init", "node");
+            grammar.AddRule("nodes:apend", "nodes node");
+
+            return grammar;
+        }
+    }
+}
